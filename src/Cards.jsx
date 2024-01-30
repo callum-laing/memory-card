bf3e334eb111bac83bfa829640ad0c90ae23eb3e
Callum Laing <callum.laing91@hotmail.co.uk>
2024-01-30 23:18:41 +0000

    have added card functionality so they randomly swap

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,13 +1,34 @@
+import React, { useState } from "react";
 import "./Cards.css";
 
 const Cards = () => {
+  const initialCards = [1, 2, 3, 4, 5];
+
+  const [currentArray, setCurrentArray] = useState(initialCards);
+
+  const shuffleCards = () => {
+    const shuffledArray = [...currentArray];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    setCurrentArray(shuffledArray);
+  };
+
   return (
     <div className="cardContainer">
-      <div className="cardBasic">Card 1</div>
-      <div className="cardBasic">Card 2</div>
-      <div className="cardBasic">Card 3</div>
-      <div className="cardBasic">Card 4</div>
-      <div className="cardBasic">Card 5</div>
+      {currentArray.map((card, index) => (
+        <div
+          key={index}
+          className={`cardBasic card${index + 1}`}
+          onClick={shuffleCards}
+        >
+          {card}
+        </div>
+      ))}
     </div>
   );
 };
